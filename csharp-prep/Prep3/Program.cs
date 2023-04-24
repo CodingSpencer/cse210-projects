1a76c9c14610f9f297626c8a96523c02e09e75a4
Spencer Ashcraft <sashcraft121@gmail.com>
2023-04-24 12:23:10 -0600

    guessing number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Ask for a number from user
+        Console.WriteLine("What is your magic number?");
+        // Change userinput to an interger
+        string userInput = Console.ReadLine();
+        int num = int.Parse(userInput);
+
+        int guess;
+        do
+        {
+            Console.WriteLine("What is your guess?");
+            string userInput2 = Console.ReadLine();
+            guess = int.Parse(userInput2);
+
+            if (guess > num)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            if (guess < num)
+            {
+                Console.WriteLine("Lower");
+            }
+        }while (guess != num);
+
+        Console.WriteLine("You guessed it!");
     }
 }
