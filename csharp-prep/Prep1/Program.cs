4c8c6854de62a634c7918f9b0c44691acd75f05e
Spencer Ashcraft <sashcraft121@gmail.com>
2023-04-24 11:40:08 -0600

    Comments added

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,13 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Ask for user's first name
         Console.Write("What is your first name? ");
         string first_name = Console.ReadLine();
 
+        // Ask for user's last name
         Console.Write("What is your last name? ");
         string last_name = Console.ReadLine();
 
+        // Spacer
         Console.WriteLine("");
+
+        // Print name in James Bond format
         Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
