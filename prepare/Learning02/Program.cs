a3486c425dabdaf2f78a46b3eb40f8d4eae9d04f
Spencer Ashcraft <sashcraft121@gmail.com>
2023-05-01 22:11:57 -0600

    defined job1 and job2 called resume class called myResume display

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company ="Microsoft";
+        job1._jobtitle = "Programmer";
+        job1._startYear = 2016;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._company = "Google";
+        job2._jobtitle = "Secrity";
+        job2._startYear = 2010;
+        job2._endYear = 2016;
+
+        Resume myResume = new Resume();
+        myResume.name = "Steve Rogers";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
     }
 }
