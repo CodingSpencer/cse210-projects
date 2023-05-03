37a76bfb4418df7fe1f472c2716b25b69432c7aa
Spencer Ashcraft <sashcraft121@gmail.com>
2023-05-03 12:28:11 -0600

    began adding prompts started get random prompt function

---
+++ prove/Develop02/Prompt.cs
@@ -0,0 +1,20 @@
+public class Prompt
+{
+
+    List<string> _usedPrompts;
+    List<string> _prompts;
+
+    public Prompt()
+    {
+       _prompts.Add("How was your day?");
+       _prompts.Add("");     
+    }
+
+    public string GetRandomPrompt()
+    {
+        return "This is a prompt";
+    }
+
+
+
+}
