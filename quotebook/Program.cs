d17bebda2ac6a66b685ddeaf6d55fe1a9b0c6886
Spencer Ashcraft <sashcraft121@gmail.com>
2023-05-05 15:24:41 -0600

    update

--- quotebook/Program.cs
+++ quotebook/Program.cs
@@ -1,6 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
 
-Source source = new Source("1 Nephi 1:1");
+Source source = new Source("None Were With Him", "http://churchofjesuschrist.org");
 
 Console.WriteLine(source.Stringify());
+
+/*Quote holland = new Quote("Elder Holland", "Because Jesus walked such a long, lonely path utterly alone, we do not have to do so".source);
+
+// Console.WriteLine(holland.GetQuote());
+
+Board quoteboard = new Board();
+quoteboard.AddQuote(holland);
+quoteboard.GetRandomQuote();*/
