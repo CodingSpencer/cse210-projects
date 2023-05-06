f7ea14b739a31112319aa379a452e581ed50680b
Spencer Ashcraft <sashcraft121@gmail.com>
2023-05-05 22:25:52 -0600

    calls the classes calls and creates the board calls menu and display

--- quotebook/Program.cs
+++ quotebook/Program.cs
@@ -1,14 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
+Board _board = new Board();
 
-Source source = new Source("None Were With Him", "http://churchofjesuschrist.org");
+_board.AddQuote(
+    new Quote(
+        "Paul",
+        "God hath not given us the spirit of fear; but of power, and of love, and of a sound mind.",
+        new Source("2 Timothy 1:7")
+    )
+);
 
-Console.WriteLine(source.Stringify());
+_board.AddQuote(
+    new Quote(
+        "Sister Elaine S. Dalton",
+        "Work hard to achieve your dreams. Don't let discouragement or mistakes delay you.",
+        new Source(
+            "How to Dare Great Things",
+            "https://www.churchofjesuschrist.org/study/new-era/2012/03/how-to-dare-great-things?lang=eng"
+        )
+    )
+);
 
-/*Quote holland = new Quote("Elder Holland", "Because Jesus walked such a long, lonely path utterly alone, we do not have to do so".source);
+_board.AddQuote(new Quote("Alma","If ye have faith ye hope for things which are not seen, which are true.", new Source("Alma 32:21")));
+_board.AddQuote(new Quote("Alma", "Live in thanksgiving daily.", new Source("Alma 34:38")));
+_board.AddQuote(new Quote("Alma", "Councel with the Lord in all they doings, and he will direct thee for good.", new Source("Alma 37:37")));
+
+// _board.GetRandomQuote();
+
+Menu menu = new Menu(_board);
+menu.Display();
+
+
+
+
+
+// Source source = new Source("None Were with him","https://www.churchofjesuschrist.org");
+
+// Console.WriteLine(source.Stringify());
+
+// Quote holland = new Quote("Elder Holland", "Because Jesus walked such a long, lonely path utterly alone, we do not have to do so", source);
 
 // Console.WriteLine(holland.GetQuote());
 
-Board quoteboard = new Board();
-quoteboard.AddQuote(holland);
-quoteboard.GetRandomQuote();*/
+// Board quoteboard = new Board();
+// quoteboard.AddQuote(holland);
+// quoteboard.FindQuotesByAuthor("holland");
