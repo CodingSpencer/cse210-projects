6f738e208b06d6f8ec880ebdec2c510e912f3f4c
Spencer Ashcraft <sashcraft121@gmail.com>
2023-05-11 23:40:01 -0600

    saves user inputs as a csv loads and displays entries entered into the file

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,3 +1,36 @@
+using System.IO;
+
 public class Journal{
     
+public void SaveToFile(List<string> saveEntries){
+    Console.WriteLine("Saving to file...");
+    string filename = "journal.csv";
+
+    using (StreamWriter outputfile = new StreamWriter(filename)){
+        foreach (string entry in saveEntries){
+            outputfile.WriteLine(entry);
+        }
+        }
+    }
+
+    public List<string> ReadFromFile(){
+        Console.WriteLine("Reading from file...");
+        List<string> savedEntries = new List<string>();
+        string filename = "journal.csv";
+
+        string[] lines = System.IO.File.ReadAllLines(filename);
+
+        foreach (string line in lines){
+            Console.WriteLine(line);
+        }
+
+        return savedEntries;
+        
+    }
+
+    
 }
+
+
+
+
