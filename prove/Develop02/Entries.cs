dbeca0c891eba36f3df41047ab45e1a8a238a325
Spencer Ashcraft <sashcraft121@gmail.com>
2023-05-11 23:42:12 -0600

    creates variables uses methods to display the entries class and print it from its list

--- prove/Develop02/Entries.cs
+++ prove/Develop02/Entries.cs
@@ -1,3 +1,32 @@
 public class Entries{
-    
+    public string prompt;
+
+    public string date;
+
+    public string entry;
+
+    public string rating;
+
+    public string lovedOnes;
+
+    public List<string> people = new List<string>();
+
+    public List<string> saveEntries = new List<string>();
+
+    Program program = new Program();
+
+    public string Display(){
+        return $"{date} - {prompt}\nRating: {rating} People: {lovedOnes}\nEntry: {entry}";
+    }
+
+    public void entriesList(){
+        foreach (string displayEntry in saveEntries){
+                Console.WriteLine(displayEntry);
+                }
+    }
+
+    public void PeopleList(){
+        lovedOnes = String.Join(", ", people);
+    }
+
 }
