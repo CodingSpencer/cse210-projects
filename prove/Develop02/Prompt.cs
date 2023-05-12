c9d084e2803ce357e8a240bf440fd96987dc67a9
Spencer Ashcraft <sashcraft121@gmail.com>
2023-05-11 23:43:53 -0600

    creates a list of prompts retrieves random prompts to give to user cycles the list through _usedPrompts until all are used

--- prove/Develop02/Prompt.cs
+++ prove/Develop02/Prompt.cs
@@ -17,7 +17,9 @@ void addPrompts(){
             _prompts.Add("What was the most exciting part of your day? ");     
             _prompts.Add("What did you accomplish during your day? ");
             _prompts.Add("Who were you happiest to see today? ");
-
+            _prompts.Add("What was the best memory from today? ");
+            _prompts.Add("Did you change your mind on anything today? ");
+            _prompts.Add("What did today remind you of? ");
             
         }
 
@@ -29,10 +31,10 @@ void addPrompts(){
         }
 
         var random = new Random();
-        int index = random.Next(_prompts.Count);
+        int index = random.Next(0, _prompts.Count);
         var newPrompt = _prompts[index];
-        /*_prompts.Remove(newPrompt);
-        _usedPrompts.Add(newPrompt);*/
+        _prompts.Remove(newPrompt);
+        _usedPrompts.Add(newPrompt);
         return newPrompt;
         
     }
