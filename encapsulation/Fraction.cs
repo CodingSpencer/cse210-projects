ce3142de7b9a6fbef4c4712bbc79b17a24ab476d
Spencer Ashcraft <sashcraft121@gmail.com>
2023-05-17 16:35:59 -0600

    named all variables and methods listed

--- encapsulation/Fraction.cs
+++ encapsulation/Fraction.cs
@@ -1,9 +1,51 @@
 public class Fraction
 {
-    private int numerator = 0;
-    private int denominator = 0;
+    private int _top= 0;
+    private int _bottom = 0;
     public Fraction()
     {
 
     }
+
+    public Fraction(int wholeNumber){
+
+    }
+
+    public Fraction(int top, int bottom)
+    {
+
+    }
+
+    public int GetTop()
+    {
+
+    }
+
+    public void SetTop(int top)
+    {
+
+    }
+
+    public int GetBottom()
+    {
+
+    }
+
+    public void SetBottom(int bottom)
+    {
+
+    }
+
+    public string GetFractionString()
+    {
+
+    }
+
+    public double GetDecimalValue()
+    {
+
+    }
+
+    
+
 }
