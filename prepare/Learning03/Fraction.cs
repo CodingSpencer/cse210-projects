e53cb79995d63c0279814e59539f4621dfa5ddc5
Spencer Ashcraft <sashcraft121@gmail.com>
2023-05-17 19:02:31 -0600

    receives input from program and then figures out the format to present

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -1,49 +1,55 @@
 public class Fraction
 {
-    private int _top= 0;
-    private int _bottom = 0;
+    private int _top;
+    private int _bottom;
     public Fraction()
     {
-
+        _top = 1;
+        _bottom = 1;
     }
 
-    public Fraction(int wholeNumber){
-
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
     }
 
     public Fraction(int top, int bottom)
     {
-
+        _top = top;
+        _bottom = bottom;
     }
+
+
 
     public int GetTop()
     {
-
+        return _top;
     }
 
     public void SetTop(int top)
     {
-
+        _top = top;
     }
 
     public int GetBottom()
     {
-
+        return _bottom;
     }
 
     public void SetBottom(int bottom)
     {
-
+        _bottom = bottom;
     }
 
     public string GetFractionString()
     {
-
+        return $"{_top}/{_bottom}";
     }
 
     public double GetDecimalValue()
     {
-
+        return (double)_top/(double)_bottom;
     }
 
     
