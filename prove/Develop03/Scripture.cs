135bee32ba50e63468112b6c940b12e627a1f258
Spencer Ashcraft <sashcraft121@gmail.com>
2023-05-24 16:46:49 -0600

    updated

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,3 +1,22 @@
 public class Scripture{
-    
+    private string _text = "Jesus Wept.";
+
+    //List<string> words = new List<string>();
+
+    public void makingList(){
+        string[] words = _text.Split(" ");
+        //string cleanWords = words.Remove
+        foreach (string word in words)
+    {
+        foreach (char letter in word){
+            if (Char.IsLetter(letter) == false){
+                word.Remove()
+            }
+            else{
+
+            }
+        }
+    }
+    }
+
 }
