7496f889a478c7c8aca3ce6fd4be44b40a21a23a
Spencer Ashcraft <sashcraft121@gmail.com>
2023-05-26 14:16:21 -0600

    has two lists one turns text to a list other keeps track of which words are hidden hides words in the scripture

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,15 +1,14 @@
 public class Scripture{
-    private string _text = "Jesus Wept.";
 
-    List<Word> wordList = new List<Word>();
+    List<Word> _wordList = new List<Word>();
 
-    List<int> Num = new List<int>();
+    public List<int> Num = new List<int>();
 
     public Scripture(string scripture){
         int counter = 0;
         string[] words = scripture.Split(" ");
         foreach (string word in words){
-            wordList.Add(new Word(word));
+            _wordList.Add(new Word(word));
             Num.Add(counter);
             counter += 1;
         }
@@ -20,15 +19,15 @@ public class Scripture{
             var random = new Random();
             int index = random.Next(0, Num.Count);
             if (Num.Count() != 0){
-            wordList[Num[index]].hideWord();
-            Num.RemoveAt(index);
-        }
+                _wordList[Num[index]].hideWord();
+                Num.RemoveAt(index);
+            }
     }
     }
 
     public void DisplayScripture(){
-        foreach (Word word in wordList){
+        foreach (Word word in _wordList){
             word.Display();
-        }
+        } 
     }
 }
