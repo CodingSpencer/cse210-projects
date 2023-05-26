99776741845905b49b66f513f305a99d1d49f867
Spencer Ashcraft <sashcraft121@gmail.com>
2023-05-26 14:36:43 -0600

    added the multiple scriptures

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Reference reference = new Reference("Alma", 48, 17);
-        Scripture scripture = new Scripture("Yea, verily, verily I say unto you, if all men had been, and were, and ever would be, like unto Moroni, behold, the very powers of hell would have been shaken forever; yea, the devil would never have power over the hearts of the children of men.");
-        reference.Display();
+        Reference reference = new Reference("Alma", 48, 17,18);
+        Scripture scripture = new Scripture("17 Yea, verily, verily I say unto you, if all men had been, and were, and ever would be, like unto Moroni, behold, the very powers of hell would have been shaken forever; \nyea, the devil would never have power over the hearts of the children of men.\n18 Behold, he was a man like unto aAmmon, the son of Mosiah, yea, and even the other sons of Mosiah, yea, and also Alma and his sons, for they were all men of God.");
+        reference.DisplayMultiple();
         scripture.DisplayScripture();
         while (true){
             string input = Console.ReadLine();
