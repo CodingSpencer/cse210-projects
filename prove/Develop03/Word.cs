2dca434a5e5995b51aa70c131ad55f3297e811d8
Spencer Ashcraft <sashcraft121@gmail.com>
2023-05-25 19:14:30 -0600

    divides list into words and hides them randomly

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,3 +1,25 @@
 public class Word{
-    
+    string _word;
+
+    bool _isHidden = false;
+
+    public Word(string word){
+        _word = word;
+    }
+    public void Display(){
+        if (_isHidden == false){
+            Console.Write($"{_word} ");
+        }
+        else if (_isHidden == true){
+            foreach (Char i in _word){
+                Console.Write("_");
+            }
+            Console.Write(" ");
+        }
+    }
+
+    public void hideWord(){
+        
+        _isHidden = true;
+    }
 }
