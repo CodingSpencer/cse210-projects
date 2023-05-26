fc2ce51786d6c63933348a45eb5e510ec21981dc
Spencer Ashcraft <sashcraft121@gmail.com>
2023-05-26 14:12:41 -0600

    creates instances through Scripture's and Reference's constructors calls other classes methods

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,12 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Scripture scripture = new Scripture("Captain America is the best avenger ever born.");
+        Reference reference = new Reference("Alma", 48, 17);
+        Scripture scripture = new Scripture("Yea, verily, verily I say unto you, if all men had been, and were, and ever would be, like unto Moroni, behold, the very powers of hell would have been shaken forever; yea, the devil would never have power over the hearts of the children of men.");
+        reference.Display();
         scripture.DisplayScripture();
         while (true){
-            Console.ReadLine();
-            scripture.hideScripture();
-            scripture.DisplayScripture();
+            string input = Console.ReadLine();
+            if (scripture.Num.Count() == 0){
+                Console.WriteLine("You completed memorizing the scripture!");
+                break;
+            }
+            else if (input == ""){
+                scripture.hideScripture();
+                reference.Display();
+                scripture.DisplayScripture();
+            }
+            else if (input == "quit"){
+                Console.WriteLine("Thanks for playing!");
+                break;
+            }
         }
     }
 }
