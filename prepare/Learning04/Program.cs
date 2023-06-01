55cfc6d42ca72e44b6e870aa9110e13ee6890d61
Spencer Ashcraft <sashcraft121@gmail.com>
2023-05-31 20:12:55 -0600

    creates assignments

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment multiply = new Assignment("Samuel Bennett", "Multiplication");
+        multiply.GetSummary();
+        
+        MathAssignment math1 = new MathAssignment("Robert Rodriguez", "Fractions", "Section 7.3", "Problems 8-19");
+        math1.GetHomeworkList();
+
+        WritingAssignment english1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        english1.GetWritingInformation();
     }
 }
