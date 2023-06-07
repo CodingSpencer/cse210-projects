d3e5260c1a91d40b22ad3277c3ec05fd2d431f78
Spencer Ashcraft <sashcraft121@gmail.com>
2023-06-07 12:20:45 -0600

    created all classes

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,34 @@
+public class Activity {
+    protected string _name;
+
+    protected string _desc;
+
+    protected int _sec;
+
+    string _endMessage;
+
+    public Activity() {
+
+    }
+
+    public Display() {
+
+    }
+
+    public SetSeconds() {
+
+    }
+
+    public Pause() {
+
+    }
+
+    public Countdown() {
+
+    }
+
+    public Spin() {
+
+    }
+
+}
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        //Console.WriteLine("Hello Develop04 World!");
+        while (true) {
+            Console.Writeline("Welcome to Improving your Mindfulness!");
+            Console.Writeline("Please Choose an Option Below: ");
+            Console.Writeline("1.Breathing\n2.Reflection\n3.Listing\n4.Listing");
+            Console.Writeline("What do you want to do?");
+            int response = Console.Read();
+            if (response == 1) {
+
+            }
+            else if (response == 2) {
+                
+            }
+            else if (response == 3) {
+
+            }
+            else {
+                break;
+            }
+
+
+        }
+            
     }
 }
