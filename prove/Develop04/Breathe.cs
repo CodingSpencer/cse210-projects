c365e699f1890bb66a64b0501182f63390e92061
Spencer Ashcraft <sashcraft121@gmail.com>
2023-06-10 19:29:32 -0600

    working breathe class

--- prove/Develop04/Breathe.cs
+++ prove/Develop04/Breathe.cs
@@ -2,13 +2,45 @@ public class Breathe : Activity {
     
     public Breathe() {
         _name = "Breathing";
-        _desc = "Have fun!";
+        _desc = "This activity will help  you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.";
     }
 
     public void DoActivity() {
-        Intro();
+        _sec = Intro();
         GetReady(5);
-
-
+        InAndOut();
+        Outro(_name, _sec);
     }
+
+    private void InAndOut() {
+        int _time = int.Parse(_sec);
+        DateTime startTime = DateTime.Now;
+        DateTime futureTime = startTime.AddSeconds(_time); 
+        while (true) {
+            Console.WriteLine("");
+            int i = 3;
+            Console.WriteLine("");
+            Console.Write("Breathe In...");
+            while (i != 0) {
+                Console.Write(i);
+                Thread.Sleep(1000);
+                Console.Write("\b \b");
+                i--;
+            }
+            i = 3;
+            Console.WriteLine("");
+            Console.Write("Breathe Out...");
+            while (i != 0) {
+                Console.Write(i);
+                Thread.Sleep(1000);
+                Console.Write("\b \b");
+                i--;
+            }
+            DateTime currentTime = DateTime.Now; 
+            if (currentTime >futureTime) {
+                break;
+            }
+        }
+    }
+
 }
