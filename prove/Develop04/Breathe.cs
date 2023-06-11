9051cf8e32b60935c60bc6a6870b1120e2e363ad
Spencer Ashcraft <sashcraft121@gmail.com>
2023-06-10 22:40:42 -0600

    breathing excercises

--- prove/Develop04/Breathe.cs
+++ prove/Develop04/Breathe.cs
@@ -2,14 +2,16 @@ public class Breathe : Activity {
     
     public Breathe() {
         _name = "Breathing";
-        _desc = "This activity will help  you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.";
+        _desc = "This activity will help you relax by walking through breathing in and out slowly. Clear your mind and focus on your breathing.";
     }
 
     public void DoActivity() {
         _sec = Intro();
-        GetReady(5);
+        GetReady(3);
         InAndOut();
         Outro(_name, _sec);
+        Spin(3);
+        Console.Clear();
     }
 
     private void InAndOut() {
@@ -37,10 +39,11 @@ public class Breathe : Activity {
                 i--;
             }
             DateTime currentTime = DateTime.Now; 
-            if (currentTime >futureTime) {
+            if (currentTime > futureTime) {
                 break;
             }
         }
+        Console.WriteLine("");
     }
 
 }
