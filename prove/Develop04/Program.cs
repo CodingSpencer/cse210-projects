6c152d5b147f5b78387afee58a1bd263b5ccccdb
Spencer Ashcraft <sashcraft121@gmail.com>
2023-06-10 22:40:21 -0600

    program to run the code

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,24 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Console.WriteLine("Hello Develop04 World!");
         while (true) {
             Console.WriteLine("Welcome to Improving your Mindfulness!");
+            Console.WriteLine("");
             Console.WriteLine("Please Choose an Option Below: ");
             Console.WriteLine("1.Breathing\n2.Reflection\n3.Listing\n4.Quit");
             Console.WriteLine("What do you want to do?");
             string response = Console.ReadLine();
             if (response == "1") {
                 Breathe breathe = new Breathe();
+                Console.Clear();
                 breathe.DoActivity();
             }
             else if (response == "2") {
-                
+                Reflection reflection = new Reflection();
+                Console.Clear();
+                reflection.DoActivity();
             }
             else if (response == "3") {
-
+                Listing listing = new Listing();
+                Console.Clear();
+                listing.DoActivity();
             }
             else {
+                Console.Clear();
                 break;
             }
 
