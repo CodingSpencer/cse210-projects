665070f7a96288db824380ffdc0da676f98a4b06
Spencer Ashcraft <sashcraft121@gmail.com>
2023-06-10 19:30:01 -0600

    breathing option works

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -8,17 +8,17 @@ class Program
         while (true) {
             Console.WriteLine("Welcome to Improving your Mindfulness!");
             Console.WriteLine("Please Choose an Option Below: ");
-            Console.WriteLine("1.Breathing\n2.Reflection\n3.Listing\n4.Listing");
+            Console.WriteLine("1.Breathing\n2.Reflection\n3.Listing\n4.Quit");
             Console.WriteLine("What do you want to do?");
-            int response = Console.Read();
-            if (response == 1) {
+            string response = Console.ReadLine();
+            if (response == "1") {
                 Breathe breathe = new Breathe();
                 breathe.DoActivity();
             }
-            else if (response == 2) {
+            else if (response == "2") {
                 
             }
-            else if (response == 3) {
+            else if (response == "3") {
 
             }
             else {
